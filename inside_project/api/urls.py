a4cbfcb8839f1af95343acb881fc205f5a2584fc
Павел Пацей <patsey.pavel@gmail.com>
2022-09-01 20:37:12 +0300

    пробую написать получение токена с помощью CustomTokenObtainSerializer и CustomTokenObtainView

--- inside_project/api/urls.py
+++ inside_project/api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import include, path
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+from .views import CustomTokenObtainView
 
 app_name = "api"
 
+router = DefaultRouter()
+
 urlpatterns = [
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.jwt")),
+    path("auth/token/", CustomTokenObtainView.as_view(), name="obtain_token"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
