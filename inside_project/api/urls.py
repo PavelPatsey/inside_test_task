4f4b9ab0b98cca7e7feb8c3cd70dbc0e8ad9171d
Павел Пацей <patsey.pavel@gmail.com>
2022-09-01 21:04:53 +0300

    used SlidingToken

--- inside_project/api/urls.py
+++ inside_project/api/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-from .views import CustomTokenObtainView
+from rest_framework_simplejwt.views import TokenObtainSlidingView
 
 app_name = "api"
 
-router = DefaultRouter()
 
 urlpatterns = [
-    path("auth/token/", CustomTokenObtainView.as_view(), name="obtain_token"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/", TokenObtainSlidingView.as_view(), name="token_obtain"),
 ]
