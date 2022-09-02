9ddc2b62cf29ea13a2edce8a59e1243b603fe4b0
Павел Пацей <patsey.pavel@gmail.com>
2022-09-02 23:58:17 +0300

    переписал метод get_raw_token в CustomJWTAuthentication

--- inside_project/api/authentication.py
+++ inside_project/api/authentication.py
@@ -1,7 +1,26 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.authentication import (AUTH_HEADER_TYPE_BYTES,
+                                                     AuthenticationFailed,
+                                                     JWTAuthentication)
 
 
 class CustomJWTAuthentication(JWTAuthentication):
     def get_raw_token(self, header):
-        header = header.replace(b"Bearer_", b"Bearer ")
-        return super(CustomJWTAuthentication, self).get_raw_token(header)
+        """
+        Переопределил метод, чтобы заголовке Authorization токен
+        передавался в формате Bearer_<полученный токен>.
+        """
+        parts = header.split(b"_")
+
+        if len(parts) == 0:
+            return None
+
+        if parts[0] not in AUTH_HEADER_TYPE_BYTES:
+            return None
+
+        if len(parts) != 2:
+            raise AuthenticationFailed(
+                _("Authorization header must contain two space-delimited values"),
+                code="bad_authorization_header",
+            )
+
+        return parts[1]
