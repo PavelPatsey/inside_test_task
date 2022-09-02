cff4385cf1afd86ffe87d8e09f7ae5f4d32655a6
Павел Пацей <patsey.pavel@gmail.com>
2022-09-02 10:29:26 +0300

    add Message model

--- inside_project/api/models.py
+++ inside_project/api/models.py
@@ -1,3 +1,16 @@
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
+
+
+class Message(models.Model):
+    name = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+        related_name="messages",
+    )
+    message = models.TextField(
+        verbose_name="Текст сообщения",
+    )
