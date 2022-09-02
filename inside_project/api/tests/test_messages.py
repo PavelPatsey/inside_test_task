3e653b59b37847a676609235c863df5835344dc4
Павел Пацей <patsey.pavel@gmail.com>
2022-09-02 14:42:17 +0300

    пишу создание сообщения + тесты

--- inside_project/api/tests/test_messages.py
+++ inside_project/api/tests/test_messages.py
@@ -1,9 +1,10 @@
-from django.conf import settings
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from ..models import User, Message
+from ..models import Message, User
+
+MESSAGES_NUMBER = 15
 
 
 class MessageTest(TestCase):
@@ -11,16 +12,39 @@ class MessageTest(TestCase):
     def setUpTestData(cls):
         cls.guest_client = APIClient()
 
-        cls.test_user = User.objects.create_user(username="testusername")
-        cls.test_client = APIClient()
-        cls.test_client.force_authenticate(cls.test_user)
+        cls.user = User.objects.create_user(username="authorized_user")
+        cls.authorized_client = APIClient()
+        cls.authorized_client.force_authenticate(cls.user)
+
+        objects = (
+            Message(
+                name=cls.user,
+                message="test message text %s" % i,
+            )
+            for i in range(MESSAGES_NUMBER)
+        )
+        cls.messages = Message.objects.bulk_create(list(objects))
 
     def test_cool_test(self):
         """Cool test."""
         self.assertEqual(True, True)
 
-    def test_get_recipes_list_unauthorized_user(self):
-        """Получение списка сообщений неавторизованным пользователем"""
+    def test_get_messages_list_unauthorized_user(self):
+        """Получение списка сообщений неавторизованным пользователем."""
         url = "/api/messages/"
         response = self.guest_client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_messages_list_authorized_user(self):
+        """Получение списка сообщений авторизованным пользователем."""
+        url = "/api/messages/"
+        response = self.authorized_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_message_authorized_user(self):
+        """Создание сообщения авторизованным пользователем."""
+        url = "/api/messages/"
+        name = self.user.username
+        data = {"name": name, "message": "текст сообщение"}
+        response = self.authorized_client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
