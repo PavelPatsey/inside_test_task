6a51508e7b88b5f8d71b4f9238b4125477e8fc98
Павел Пацей <patsey.pavel@gmail.com>
2022-09-02 13:51:17 +0300

    пишу MessageSerializer и APIMessage

--- inside_project/api/serializers.py
+++ inside_project/api/serializers.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import Message
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="name.username")
+
+    class Meta:
+        model = Message
+        fields = [
+            "name",
+            "message",
+        ]
+
+    def create(self, validated_data):
+        # Уберем список достижений из словаря validated_data и сохраним его
+        # breakpoint()
+        # name = validated_data.pop('name')
+
+        # Создадим нового котика пока без достижений, данных нам достаточно
+        message = Message.objects.create(**validated_data)
+        # message = Message.objects.create(
+        #     name=validated_data["name"],
+        # )
+
+        return message
--- inside_project/api/urls.py
+++ inside_project/api/urls.py
@@ -1,9 +1,22 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainSlidingView
+from django.urls import include, path
+from rest_framework_simplejwt.views import (TokenObtainSlidingView,
+                                            TokenRefreshSlidingView)
+
+from .views import APIMessage
 
 app_name = "api"
 
 
 urlpatterns = [
-    path("auth/token/", TokenObtainSlidingView.as_view(), name="token_obtain"),
+    path(
+        "auth/token/",
+        TokenObtainSlidingView.as_view(),
+        name="token_obtain",
+    ),
+    path(
+        "auth/token/refresh/",
+        TokenRefreshSlidingView.as_view(),
+        name="token_refresh",
+    ),
+    path("messages/", APIMessage.as_view()),
 ]
