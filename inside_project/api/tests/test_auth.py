33d54bb91f6c0562d97dc4ad9e74b4b11c0e426b
Павел Пацей <patsey.pavel@gmail.com>
2022-09-03 18:51:24 +0300

    подправил тесты

--- inside_project/api/tests/test_auth.py
+++ inside_project/api/tests/test_auth.py
@@ -9,23 +9,34 @@ from ..tokens import generate_access_token
 class AuthTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
+        cls.user = User.objects.create_user(username="test_user")
 
-
-class TestCaseBase(TestCase):
     def test_cool_test(self):
         """Cool test."""
         self.assertEqual(True, True)
 
+    # def test_token_obtain(self):
+    #     """Получение токеном."""
+    #     client = APIClient()
+    #     access_token = generate_access_token(user)
+    #     client.credentials(HTTP_AUTHORIZATION=f"Bearer_{access_token}")
+
+    #     url = "/api/messages/"
+    #     name = user.username
+    #     data = {"name": name, "message": "текст сообщения"}
+    #     response = client.post(url, data, format="json")
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     test_json = {"name": "test_user", "message": "текст сообщения"}
+    #     self.assertEqual(response.json(), test_json)
+    
     def test_request_header_with_token(self):
         """Проверка заголовка запроса с полученным токеном."""
-        user = User.objects.create_user(username="test_user")
         client = APIClient()
-        access_token = generate_access_token(user)
+        access_token = generate_access_token(self.user)
         client.credentials(HTTP_AUTHORIZATION=f"Bearer_{access_token}")
 
         url = "/api/messages/"
-        name = user.username
+        name = self.user.username
         data = {"name": name, "message": "текст сообщения"}
         response = client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -34,17 +45,14 @@ class TestCaseBase(TestCase):
 
     def test_request_with_incorrect_header(self):
         """Неправильный заголовок запроса с полученным токеном."""
-        user = User.objects.create_user(username="test_user")
         client = APIClient()
-        refresh = SlidingToken.for_user(user)
-        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh}")
+        access_token = generate_access_token(self.user)
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
 
         url = "/api/messages/"
-        name = user.username
+        name = self.user.username
         data = {"name": name, "message": "текст сообщения"}
         response = client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        test_json = {
-            "detail": "Authentication credentials were not provided."
-        }
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        test_json = {'detail': 'Token prefix missing'}
         self.assertEqual(response.json(), test_json)
