b904493485713d7fd2c9533c1a557cb52d964d66
Павел Пацей <patsey.pavel@gmail.com>
2022-09-03 17:33:26 +0300

    add management command fill_database_with_test_data

---
+++ inside_project/api/management/commands/fill_database_with_test_data.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+
+from ...models import Message, User
+
+OBJECTS_NUMBER = 15
+
+
+class Command(BaseCommand):
+    help = "Fill the database with test data"
+
+    def handle(self, *args, **kwargs):
+
+        test_user = User.objects.create_user(username="test_user")
+
+        objects_number = OBJECTS_NUMBER
+        objects = (
+            Message(
+                name=test_user,
+                message="test message text %s" % i,
+            )
+            for i in range(objects_number)
+        )
+        Message.objects.bulk_create(list(objects))
